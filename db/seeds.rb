a32ff5fd6c4d20540d09c4772eab1d86106ef90f
Brennan Basinger <bdbasinger777@gmail.com>
2018-09-14 06:08:47 -0500

    Create products table

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,32 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Product.delete_all
+Product.create!(title: 'LacyBPottery Product 1',
+  description:
+    %{<p>
+      <em>Beautiful Product</em>
+      Another Beautiful LacyBPottery Product!
+      </p>},
+  image_url: 'sample1.jpg',
+  price: 16.00)
+# . . .
+Product.create!(title: 'LacyBPottery Product 2',
+  description:
+    %{<p>
+      <em>Beautiful Product</em>
+      Another Beautiful LacyBPottery Product!
+      </p>},
+  image_url: 'sample2.jpg',
+  price: 22.00)
+# . . .
+
+Product.create!(title: 'LacyBPottery Product 3',
+  description:
+    %{<p>
+      <em>Beautiful Product</em>
+      Another Beautiful LacyBPottery Product!
+      </p>},
+  image_url: 'sample3.jpg',
+  price: 25.00)
